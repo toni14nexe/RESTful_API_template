6d4446f515c72f8711ec0fd05001d9b2b5bd61a0
Toni Kolaric <toni.kolaric@barrage.net>
2024-08-12 14:13:34 +0200

    Add pagination to /debts route for app v2.0.0

--- api/controllers/debts.js
+++ api/controllers/debts.js
@@ -1,79 +1,101 @@
-const Debt = require("../models/debt")
-const mongoose = require("mongoose")
+const Debt = require("../models/debt");
+const mongoose = require("mongoose");
 
-exports.get_all_debts = (req, res, next) => {
-    Debt.find()
-        .select('_id name done date description')
-        .exec()
-        .then(docs => {
-            res.status(200).json({
-                total: docs.length,
-                debts: docs
-            })
-        })
-        .catch(error => res.status(500).json({ error: error }))
-}
+exports.get_debts = (req, res, next) => {
+  const page = parseInt(req.query.page) || 1;
+  const perPage = parseInt(req.query.perPage) || 10;
+
+  Debt.find()
+    .select("_id name done date description")
+    .sort({ done: 1, _id: -1 })
+    .skip((page - 1) * perPage)
+    .limit(perPage)
+    .exec()
+    .then((docs) => {
+      Debt.countDocuments().then((count) => {
+        res.status(200).json({
+          total: count,
+          page,
+          perPage,
+          totalPages: Math.ceil(count / perPage),
+          debts: docs,
+        });
+      });
+    })
+    .catch((error) => res.status(500).json({ error: error }));
+};
 
 exports.get_one_debt = (req, res, next) => {
-    Debt.findById(req.params.debtId)
-        .select('_id name done date description')
-        .exec()
-        .then(doc => {
-            if(doc) res.status(200).json(doc)
-            else res.status(404).json({ Message: 'No valid entry found for provided ID' })
-        })
-        .catch(error => res.status(500).json({ error: error }))
-}
+  Debt.findById(req.params.debtId)
+    .select("_id name done date description")
+    .exec()
+    .then((doc) => {
+      if (doc) res.status(200).json(doc);
+      else
+        res
+          .status(404)
+          .json({ Message: "No valid entry found for provided ID" });
+    })
+    .catch((error) => res.status(500).json({ error: error }));
+};
 
 exports.post_debt = (req, res, next) => {
-    const debt = new Debt({
-        _id: new mongoose.Types.ObjectId(),
-        name: req.body.name,
-        done: req.body.done,
-        date: req.body.date,
-        description: req.body.description
+  const debt = new Debt({
+    _id: new mongoose.Types.ObjectId(),
+    name: req.body.name,
+    done: req.body.done,
+    date: req.body.date,
+    description: req.body.description,
+  });
+  debt
+    .save()
+    .then((result) => {
+      res.status(200).json({
+        message: "Debt created successfully",
+        debt: {
+          _id: result._id,
+          name: result.name,
+          done: result.done,
+          date: result.date,
+          description: result.description,
+        },
+      });
     })
-    debt.save()
-        .then(result => {
-            res.status(200).json({
-                message: 'Debt created successfully',
-                debt: {
-                    _id: result._id,
-                    name: result.name,
-                    done: result.done,
-                    date: result.date,
-                    description:result.description
-                }
-            })
-        })
-        .catch(error => res.status(500).json({ error: error }))
-}
+    .catch((error) => res.status(500).json({ error: error }));
+};
 
 exports.patch_debt = (req, res, next) => {
-    Debt.findById(req.params.debtId)
-        .select('_id name done date description')
-        .exec()
-        .then(doc => {
-            if(doc) {
-                Debt.updateOne({ _id: req.params.debtId }, { $set: { done: !doc.done } })
-                    .exec()
-                    .then(result => {
-                        res.status(200).json({
-                            message: 'Done status changed successfully',
-                            debt: { ...doc._doc, done: !doc.done }
-                        })
-                    })
-                    .catch(error => res.status(500).json({ error: error }))
-            }
-            else res.status(404).json({ Message: 'No valid entry found for provided ID' })
-        })
-}
+  Debt.findById(req.params.debtId)
+    .select("_id name done date description")
+    .exec()
+    .then((doc) => {
+      if (doc) {
+        Debt.updateOne(
+          { _id: req.params.debtId },
+          { $set: { done: !doc.done } }
+        )
+          .exec()
+          .then((result) => {
+            res.status(200).json({
+              message: "Done status changed successfully",
+              debt: { ...doc._doc, done: !doc.done },
+            });
+          })
+          .catch((error) => res.status(500).json({ error: error }));
+      } else
+        res
+          .status(404)
+          .json({ Message: "No valid entry found for provided ID" });
+    });
+};
 
 exports.delete_debt = (req, res, next) => {
-    Debt.deleteOne({ _id: req.params.debtId })
-        .exec()
-        .then(result => res.status(200).json({
-            message: 'Debt was deleted successfully'
-        }))
-        .catch(error => res.status(500).json({ error: error }))
-}
+  Debt.deleteOne({ _id: req.params.debtId })
+    .exec()
+    .then((result) =>
+      res.status(200).json({
+        message: "Debt was deleted successfully",
+      })
+    )
+    .catch((error) => res.status(500).json({ error: error }));
+};
--- api/routes/debts.js
+++ api/routes/debts.js
@@ -1,16 +1,16 @@
-const express = require('express')
-const router = express.Router()
-const authCheck = require("../middleware/authCheck")
-const debtsController = require("../controllers/debts")
+const express = require("express");
+const router = express.Router();
+const authCheck = require("../middleware/authCheck");
+const debtsController = require("../controllers/debts");
 
-router.get('/', debtsController.get_all_debts)
+router.get("/", debtsController.get_debts);
 
-router.get('/:debtId', debtsController.get_one_debt)
+router.get("/:debtId", debtsController.get_one_debt);
 
-router.post('/', authCheck, debtsController.post_debt)
+router.post("/", authCheck, debtsController.post_debt);
 
-router.patch('/:debtId', authCheck, debtsController.patch_debt)
+router.patch("/:debtId", authCheck, debtsController.patch_debt);
 
-router.delete('/:debtId', authCheck, debtsController.delete_debt)
+router.delete("/:debtId", authCheck, debtsController.delete_debt);
 
-module.exports = router
+module.exports = router;
